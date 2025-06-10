6a0feae668225c68f501b3d99d2f635b0dec7c2b
Priyani <priyanirrawal@gmail.com>
2025-06-10 13:07:20 -0400

    combined files

--- landing/sticky_note.py
+++ landing/sticky_note.py
@@ -1,7 +1,25 @@
 import cv2
 import numpy as np
 from PIL import Image
-from util import get_limits  # assumes it works with BGR input
+
+def get_limits(color):
+    c = np.uint8([[color]])  # BGR values
+    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
+
+    hue = hsvC[0][0][0]  # Get the hue value
+
+    # Handle red hue wrap-around
+    if hue >= 165:  # Upper limit for divided red hue
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([180, 255, 255], dtype=np.uint8)
+    elif hue <= 15:  # Lower limit for divided red hue
+        lowerLimit = np.array([0, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+    else:
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+
+    return lowerLimit, upperLimit
 
 cyan = [255, 255, 0]  # cyan in BGR
 cap = cv2.VideoCapture(0)
