1d05f16e28cd8e26f0b243a2cdda6b865dd2854f
priyanirawal <116110283+priyanirawal@users.noreply.github.com>
2025-07-03 11:44:38 -0400

    change path

---
+++ jamd/landing/sticky_note.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+from PIL import Image
+
+def get_limits(color):
+    c = np.uint8([[color]])  # BGR values
+    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
+
+    hue = hsvC[0][0][0]  # Get the hue value
+
+    # Handle red hue wrap-around
+    if hue >= 165:  # Upper limit for divided red hue
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([180, 255, 255], dtype=np.uint8)
+    elif hue <= 15:  # Lower limit for divided red hue
+        lowerLimit = np.array([0, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+    else:
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+
+    return lowerLimit, upperLimit
+
+cyan = [255, 255, 0]  # cyan in BGR
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lowerLimit, upperLimit = get_limits(color=cyan)  # or manually set as below
+    # lowerLimit = np.array([80, 100, 100])
+    # upperLimit = np.array([100, 255, 255])
+
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+    mask_ = Image.fromarray(mask)
+    bbox = mask_.getbbox()
+
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
